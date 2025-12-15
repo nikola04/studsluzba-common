31e4c31dbd8f98bc3b7f2c05041989c28e0b65c2
Nikola Vasiljevic <nvasiljevic6824si@raf.rs>
2025-12-15 23:57:19 +0100

    add skolska godina response dto

---
+++ src/main/java/org/raflab/studsluzbacommon/dto/response/SkolskaGodinaResponseDTO.java
@@ -0,0 +1,10 @@
+package org.raflab.studsluzbacommon.dto.response;
+
+import lombok.Data;
+
+@Data
+public class SkolskaGodinaResponseDTO {
+    private Long id;
+    private Integer godina;
+    private Boolean aktivan;
+}
