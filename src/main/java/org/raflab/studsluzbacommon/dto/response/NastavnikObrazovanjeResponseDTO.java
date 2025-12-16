35375f962bf2807040ebe9cde4b1c986af13ed87
Nikola Vasiljevic <nvasiljevic6824si@raf.rs>
2025-12-17 00:37:40 +0100

    DTOs added

---
+++ src/main/java/org/raflab/studsluzbacommon/dto/response/NastavnikObrazovanjeResponseDTO.java
@@ -0,0 +1,10 @@
+package org.raflab.studsluzbacommon.dto.response;
+
+import lombok.Data;
+
+@Data
+public class NastavnikObrazovanjeResponseDTO {
+    private Long nastavnikId;
+    private Long visokoskolskaUstanovaId;
+    private Long vrstaStudijaId;
+}
---
+++ src/main/java/org/raflab/studsluzbacommon/dto/response/NastavnikResponseDTO.java
@@ -0,0 +1,30 @@
+package org.raflab.studsluzbacommon.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NastavnikResponseDTO {
+    @NonNull
+    private Long id;
+    @NonNull
+    private String ime;
+    private String prezime;
+    private String srednjeIme;
+    private String email;
+    private String brojTelefona;
+    private String adresa;
+    private Set<ZvanjeResponseDTO> zvanja;
+    private Set<NastavnikObrazovanjeResponseDTO> obrazovanja;
+
+    private LocalDate datumRodjenja;
+    private Character pol;
+    private String jmbg;
+}
