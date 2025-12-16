a643f1f4d163e6a7fef6d82345b4df025ed6de1d
Nikola Nedeljkovic <nikolanedeljkovic.official@gmail.com>
2025-12-16 23:57:26 +0100

    Added SkolskaGodinaRequestDTO

---
+++ src/main/java/org/raflab/studsluzbacommon/dto/request/SkolskaGodinaRequestDTO.java
@@ -0,0 +1,9 @@
+package org.raflab.studsluzbacommon.dto.request;
+
+import lombok.Data;
+
+@Data
+public class SkolskaGodinaRequestDTO {
+    private Integer godina;
+    private Boolean aktivan;
+}
